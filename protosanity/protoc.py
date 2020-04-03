3c4d4d7aadea1aac057f768379862152ad0e086b
Michael McDermott <michael.mcdermott@kitware.com>
2020-04-03 14:43:21 -0400

    added protoc dropin from other branch

---
+++ protosanity/protoc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from grpc_tools import protoc
+
+
+def protoc_main_minimal(command_arguments):
+    """
+    Same as `python -m grpc_tools.protoc` but without injecting
+    ['-I{}'.format(proto_include)].
+    This is a fix to be able to actually compile grpc proto files correctly when import is involved.
+    """
+    sys.exit(protoc.main(command_arguments))
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1:
+        sys.stderr.write('Must provide arguments to protoc. Type `python -m protosanity.protoc -h` to show help\n')
+        sys.exit(protoc.main(sys.argv[0]))
+
+    protoc_main_minimal(sys.argv[1:])
+
